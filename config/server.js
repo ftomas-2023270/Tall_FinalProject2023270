debd248135d51dc8b201e08d2ead4eb3b9b9105c
ftomas <ftomas-2023270@kinal.edu.gt>
2025-02-24 07:39:59 -0600

    Se agrego el 35% del proyecto (inicializacion del servidor y la conexion a la base de datos)

---
+++ config/server.js
@@ -0,0 +1,44 @@
+'use strict';
+
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import {dbConnection} from './mongo.js';
+import limiter from '../src/middleware/validar-cant-peticiones.js';
+
+const middlewares = (app)=>{
+    app.use(express.urlencoded({extended:false}));
+    app.use(cors());
+    app.use(express.json());
+    app.use(helmet());
+    app.use(morgan('dev'));
+    app.use(limiter);
+}
+
+const routes = (app) =>{
+}
+
+const conectarDB = async()=>{
+    try {
+        await dbConnection();
+        console.log('Conexion a la base de datos exitosa');
+    } catch (error) {
+        console.error('Error conectando a la base de datos',error);
+        process.exit(1);
+    }
+}
+
+export const initServer= async()=>{
+    const app = express();
+    const port = process.env.port || 3333;
+
+    try {
+        middlewares(app);
+        conectarDB();
+        app.listen(port);
+        console.log(`Server running on port ${port}`)
+    } catch (e) {
+        console.log(`Server init failed: ${e}`)
+    }
+}
---
+++ index.js
@@ -0,0 +1,7 @@
+import { config } from "dotenv";
+
+config();
+
+import { initServer } from "./config/server.js";
+
+initServer();
